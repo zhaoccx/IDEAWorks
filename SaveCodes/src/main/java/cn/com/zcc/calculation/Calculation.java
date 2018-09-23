7fb07423d0e84ef61ecbf8cb1bd2709830247571
趙常春 <zhaoccx@hotmail.com>
2018-09-23 13:03:17 +0800

    test init.

--- SaveCodes/src/main/java/cn/com/zcc/calculation/Calculation.java
+++ SaveCodes/src/main/java/cn/com/zcc/calculation/Calculation.java
@@ -1,50 +1,74 @@
 package cn.com.zcc.calculation;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
+import java.util.Map;
+
+/**
+ * 简单的复利计算
+ */
 public class Calculation {
 
-    public static void main(String[] args) {
-        System.out.println("OK-----Start");
-        Map<Double, Double> concurrentHashMap = new ConcurrentHashMap<Double, Double>();
-        double calcs = new Calculation().getCalcs(45000, 20, 5, concurrentHashMap);
-        System.out.println("计算结果为："+calcs);
-        System.out.println(concurrentHashMap);
-        System.out.println("计算结果为："+new Calculation().getAverage(concurrentHashMap));
 
-        System.out.println("OK-----End");
-
-    }
-
-    public double getCalcs(double init, double rate, double year, Map<Double, Double> doublemap){
+    /**
+     * 迭代法
+     * @param init
+     * @param rate
+     * @param year
+     * @param doublemap
+     * @return
+     */
+    public double getCalcs(double init, double rate, int year, Map<String, Double> doublemap){
         if (year<=0){
             return init;
         }
 
         if (year>1){
-            doublemap.put(year,getCalcs(init,rate,year-1, doublemap)*(100+rate)/100);
+            doublemap.put(String.valueOf(year),getCalcs(init,rate,year-1, doublemap)*(100+rate)/100);
             System.out.println("第"+year+"年的本息共"+getCalcs(init,rate,year-1, doublemap));
             return getCalcs(init,rate,year-1, doublemap)*(100+rate)/100;
         }
-        doublemap.put(year,init+init*rate/100);
+        doublemap.put(String.valueOf(year),init+init*rate/100);
 
         return  init+init*rate/100;
     }
 
 
-    public Map<Double,Double> getAverage(Map<Double,Double> initMap){
-        for (Map.Entry<Double, Double> entry : initMap.entrySet()) {
-            if (entry.getKey() == 1.0) {
+    public Map<String,Double> getAverage(Map<String,Double> initMap){
+        for (Map.Entry<String, Double> entry : initMap.entrySet()) {
+            if ("1.0".equals(entry.getKey())) {
                 System.out.println("ssssssssssssssssssssssssss");
                 initMap.put(entry.getKey(), 9000D / 15);
             }else {
-                // TODO entry.getKey()是double比较的问题..................
-                initMap.put(entry.getKey(), (initMap.get(entry.getKey()+1.0D) - initMap.get(entry.getKey()) )/ 15);
+
+                System.out.println(entry.getKey());
+              //initMap.put(entry.getKey(), (initMap.get(entry.getKey()) - initMap.get(String.valueOf(Double.valueOf(entry.getKey()) - 1.0D))) / 15);
 
 
             }
         }
         return initMap;
+    }
+
+    /**
+     * 循环法
+     * @param init
+     * @param rate
+     * @param year
+     * @param doublemap
+     * @return
+     */
+    public double Calcsforeach(Double init, Double rate, int year, Map<Integer, Salarys> doublemap){
+      //  doublemap.put(0,new Salarys(0,init,0D));
+
+        for (int i = 1; i <= year; i++) {
+
+
+            doublemap.put(i,new Salarys(i,init*rate/100+init,10,init));
+            init += init*rate/100;
+
+        }
+
+
+        return  init;
     }
 }
---
+++ SaveCodes/src/main/java/cn/com/zcc/calculation/Salarys.java
@@ -0,0 +1,102 @@
+package cn.com.zcc.calculation;
+
+public class Salarys {
+
+    private Integer year;//第几年
+    private Double allSalarys;//总金额
+    private Integer averageMenthy;//平均月份
+    private Double averageSalary;//每月平均利息
+    private Double initSalary;//上年总金额
+    private Double interest;//当年总利息
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Double getAllSalarys() {
+        return allSalarys;
+    }
+
+    public void setAllSalarys(Double allSalarys) {
+        this.allSalarys = allSalarys;
+    }
+
+    public Integer getAverageMenthy() {
+        return averageMenthy;
+    }
+
+    public void setAverageMenthy(Integer averageMenthy) {
+        this.averageMenthy = averageMenthy;
+    }
+
+    public Double getAverageSalary() {
+        return averageSalary;
+    }
+
+    public void setAverageSalary(Double averageSalary) {
+        this.averageSalary = averageSalary;
+    }
+
+    public Double getInitSalary() {
+        return initSalary;
+    }
+
+    public void setInitSalary(Double initSalary) {
+        this.initSalary = initSalary;
+    }
+
+    public Double getInterest() {
+        return interest;
+    }
+
+    public void setInterest(Double interest) {
+        this.interest = interest;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "第" + year +
+                "年, 总金额=" + allSalarys +
+                ", 平均月份=" + averageMenthy +
+                ", 每月平均利息=" + averageSalary +
+                ", 上年总金额=" + initSalary +
+                ", 当年总利息=" + interest +
+                '}';
+    }
+
+    public Salarys() {
+    }
+
+    /**
+     *
+     * @param year 年份
+     * @param allSalarys 当年总金额
+     * @param initSalary 上年总金额
+     */
+    public Salarys(Integer year, Double allSalarys, Double initSalary) {
+        this.year = year;
+        this.allSalarys = allSalarys;
+        this.initSalary = initSalary;
+        if (null == this.getAverageMenthy()){
+            this.setAverageMenthy(12);
+        }
+
+        this.setInterest(this.getAllSalarys()-this.getInitSalary());
+        this.setAverageSalary(this.getInterest()/this.getAverageMenthy());
+
+    }
+
+    public Salarys(Integer year, Double allSalarys, Integer averageMenthy, Double initSalary) {
+        this.year = year;
+        this.allSalarys = allSalarys;
+        this.averageMenthy = averageMenthy;
+        this.initSalary = initSalary;
+        this.setInterest(this.getAllSalarys()-this.getInitSalary());
+        this.setAverageSalary(this.getInterest()/this.getAverageMenthy());
+    }
+}
